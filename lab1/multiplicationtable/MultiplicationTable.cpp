dc9a88880ac5b8bf452333d7b05456463044d103
matematyk60 <mleko321>
2017-03-07 18:37:33 +0100

    Rejected commit: Default

---
+++ lab1/multiplicationtable/MultiplicationTable.cpp
@@ -0,0 +1,19 @@
+//
+// Created by matematyk60 on 06.03.17.
+//
+
+#include "MultiplicationTable.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+void MultiplicationTable(int tab[][10]){
+    for(int i = 0 ; i< 10 ; i++){
+        for(int j = 0 ; j < 10 ; j++){
+            *(*(tab+i)+j) = (i+1)*(j+1);
+        }
+    }
+}
+
