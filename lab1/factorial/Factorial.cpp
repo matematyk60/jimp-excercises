adf07d390c5fa37fb6c3c52f67dfb4fdcccb5e32
matematyk60 <mleko321>
2017-03-07 18:56:37 +0100

    Rejected commit: Default

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,28 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+    if (value > 12 || value < -13) {
+        return 0;
+    }
+
+    if (value > -1) {
+        if (value == 0 || value == 1) {
+            return 1;
+        }
+
+        int answer = value;
+
+        while (value > 1) {
+            answer = answer * --value;
+        }
+
+        return answer;
+    } else {
+        if (value == -1) { return -1; }
+        int answer = value;
+        while (value < -1) {
+            answer = answer * ++value;
+        }
+        return answer;
+    }
 }
