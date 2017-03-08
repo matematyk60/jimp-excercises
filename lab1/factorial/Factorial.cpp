a03819f3bd3b3b5b1015a3b228429530be5ce2fd
matematyk60 <mleko321>
2017-03-08 12:42:14 +0100

    Changed range of factorial, cleaned i = i*p for i *= p, changed if,else for shorter one code.

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,28 +4,26 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-    if (value > 12 || value < -13) {
+    if (value > 12 || value < -12) {
         return 0;
     }
 
-    if (value > -1) {
-        if (value == 0 || value == 1) {
-            return 1;
-        }
+    int sign = 1;
 
-        int answer = value;
-
-        while (value > 1) {
-            answer = answer * --value;
-        }
-
-        return answer;
-    } else {
-        if (value == -1) { return -1; }
-        int answer = value;
-        while (value < -1) {
-            answer = answer * ++value;
-        }
-        return answer;
+    if (value < 0) {
+        sign = -1;
     }
+
+    if (value == 0 || value == 1 || value == -1) {
+        return 1 * sign;
+    }
+
+    int answer = value;
+
+    while (value > 1 || value < -1) {
+        value -= sign;
+        answer *= value;
+    }
+
+    return answer;
 }
