b72e8f7fa98cebac5a6c1c52e6e14662f9e8d1a8
Janek <ignatowicz@student.agh.edu.pl>
2017-03-12 14:23:08 +0100

    2

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -1,8 +1,21 @@
 //
-// Created by mwypych on 02.02.17.
+// Created by jan on 02.02.17.
 //
 #include "Factorial.h"
 
-int factorial(int value) {
-  return 0;
+long long int factorial(int value) {
+  long long int suma = 1;
+  if (value == 0 || value == 1) return 1;
+  else if(value >= 13) return 0;
+  else if (value > 1) {
+    for (int i = 2; i <= value; i++) {
+      suma = suma * i;
+    }
+    return suma;
+  } else if (value < 0) {
+    for (int i = value; i < 0; i++) {
+      suma = suma * i;
+    }
+    return suma;
+  }
 }
