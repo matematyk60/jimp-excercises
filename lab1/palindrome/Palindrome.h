ae336f5dff306624bcca48a47e66d566a836a252
Janek <ignatowicz@student.agh.edu.pl>
2017-03-12 13:58:15 +0100

    2

---
+++ lab1/palindrome/Palindrome.h
@@ -0,0 +1,14 @@
+//
+// Created by janek on 12.03.17.
+//
+
+#ifndef JIMP_EXERCISES_PALINDROME_H
+#define JIMP_EXERCISES_PALINDROME_H
+
+
+class Palindrome {
+
+};
+
+
+#endif //JIMP_EXERCISES_PALINDROME_H
