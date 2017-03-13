adf916ca630e3c2e2deb46d71727fb4dc47b5d78
Janek <ignatowicz@student.agh.edu.pl>
2017-03-13 07:30:49 +0100

    2

--- lab1/doublebasepalindromes/DoubleBasePalindromes.h
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -1,14 +1,12 @@
 //
-// Created by janek on 13.03.17.
+// Created by jan on 07.03.17.
 //
 
 #ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
 #define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
 
+#include <cstdint>
 
-class DoubleBasePalindromes {
-
-};
-
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
 
 #endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
