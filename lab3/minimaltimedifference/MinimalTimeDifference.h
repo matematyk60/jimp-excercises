347ed936f7751f87d316595bad4b9d9e26400362
Janek <ignatowicz@student.agh.edu.pl>
2017-03-14 18:41:00 +0100

    2

---
+++ lab3/minimaltimedifference/MinimalTimeDifference.h
@@ -0,0 +1,14 @@
+//
+// Created by janek on 14.03.17.
+//
+
+#ifndef JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+#define JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+
+
+class MinimalTimeDifference {
+
+};
+
+
+#endif //JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
