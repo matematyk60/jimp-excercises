292f2ff2098d90b031a21bede7312560b20d2cb2
Janek <ignatowicz@student.agh.edu.pl>
2017-03-14 12:13:40 +0100

    2

---
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -0,0 +1,65 @@
+//
+// Created by janek on 13.03.17.
+//
+
+#include "XorCypherBreaker.h"
+#include <algorithm>
+#include <iostream>
+using std::find;
+using std::vector;
+using std::string;
+
+
+
+
+string XorCypherBreaker(const std::vector<char> &cryptogram,
+                       int key_length,
+                       const std::vector<string> &dictionary){
+
+
+    char arr[3];
+    string str="", str1="", str2="";
+    int counter1 = 0, counter2 = 0;
+    int ite;
+    for(int i=97; i<=122; i++){
+        arr[0] = char(i);
+        //cout << arr[0] << endl;
+        for(int j=97; j<=122; j++){
+            arr[1] = char(j);
+            for(int k=97; k<=122; k++){
+
+                arr[2] = char(k);
+
+                ite = 0;
+
+                //cout << arr << "  " << str1 << endl;
+                str1="";
+                for(const auto &n : cryptogram) {
+                    if(ite == 3)
+                        ite = 0;
+                    str1 = str1 + (char)(n xor arr[ite]);
+
+                    ite++;
+                }
+
+
+                counter2 = 0;
+                for(const string &n : dictionary) {
+                    if (str1.find(n) != string::npos) {
+                        counter2++;
+
+                    }
+                }
+
+
+                if(counter2 > counter1) {
+                    str = arr;
+                    counter1 = counter2;
+                    str2 = str1;
+
+                }
+            }
+        }
+    }
+    return str;
+}
---
+++ lab2/xorcypherbreaker/XorCypherBreaker.h
@@ -0,0 +1,16 @@
+//
+// Created by janek on 13.03.17.
+//
+
+#ifndef JIMP_EXERCISES_XORCYPHERBREAKER_H
+#define JIMP_EXERCISES_XORCYPHERBREAKER_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+std::string XorCypherBreaker(const std::vector<char> &cryptogram,
+                             int key_length,
+                             const std::vector<string> &dictionary);
+
+#endif //JIMP_EXERCISES_XORCYPHERBREAKER_H
