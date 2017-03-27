6bf30db0127be6676ab05f234d9fbe6c83512c00
Janek <ignatowicz@student.agh.edu.pl>
2017-03-27 19:17:38 +0200

    jan

--- lab4/netsjson/SimpleJson.cpp
+++ lab4/netsjson/SimpleJson.cpp
@@ -2,48 +2,59 @@
 // Created by janek on 27.03.17.
 //
 
-#include "SimpleJson.h"
-#include <experimental/optional>
 #include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include "SimpleJson.h"
+#include <sstream>
+
+using ::std::vector;
+using ::std::map;
+using ::std::cout;
+using ::std::endl;
+using ::std::string;
+using ::nets::JsonValue;
+using ::std::literals::operator""s;
+using ::std::stringstream;
 
 namespace nets{
-    JsonValue::JsonValue() {
+
+    JsonValue::JsonValue(int numberInt2){
+        numberInt = numberInt2;
     }
 
-    JsonValue::JsonValue(int numberInt){
-
+    JsonValue::JsonValue(double numberDouble2){
+        numberDouble = numberDouble2;
     }
 
-    JsonValue::JsonValue(double numberDouble){
-
+    JsonValue::JsonValue(string str2){
+        str = str2;
     }
 
-    JsonValue::JsonValue(std::string str){
-
+    JsonValue::JsonValue(bool tf2){
+        tf = tf2;
     }
 
-    JsonValue::JsonValue(bool tf){
-
+    JsonValue::JsonValue(vector <JsonValue> js2){
+        js = js2;
     }
 
-    JsonValue::JsonValue(JsonValue sth){
-
-    }
-
-    JsonValue::JsonValue(std::array arr){
-
-    }
-
-    JsonValue::~JsonValue() {
+    JsonValue::JsonValue(map <string, JsonValue> arr2){
+        arr = arr2;
     }
 
 
-    std::experimental::optional<JsonValue> JsonValue::ValueByName(const std::string &name) const{
+    std::experimental::optional<JsonValue> JsonValue::ValueByName(const string &name) const{
 
     }
 
-    std::string JsonValue::ToString() const{
-
+    string JsonValue::ToString() const{
+        string tmp;
+        stringstream ss;
+        //ss << "{" <<  << ":" <<  << "}";
+        ss >> tmp;
+        return tmp;
     }
 
 
--- lab4/netsjson/SimpleJson.h
+++ lab4/netsjson/SimpleJson.h
@@ -7,36 +7,46 @@
 
 #include <experimental/optional>
 #include <string>
-#include <array>
+#include <vector>
+#include <map>
+using ::std::map;
+using ::std::string;
+using ::std::vector;
 
 namespace nets{
     class JsonValue{
     public:
 
-        JsonValue();
+        JsonValue(){};
 
-        JsonValue(int numberInt);
+        ~JsonValue(){};
 
-        JsonValue(double numberDouble);
+        JsonValue(int numberInt2);
 
-        JsonValue(std::string str);
+        JsonValue(double numberDouble2);
 
-        JsonValue(bool tf);
+        JsonValue(string str2);
 
-        JsonValue(JsonValue sth);
-
-        JsonValue(std::array arr);
-
-        ~JsonValue();
+        JsonValue(bool tf2);
 
 
-        std::experimental::optional<JsonValue> ValueByName(const std::string &name) const;
+        JsonValue(vector <JsonValue> js2);
 
-        std::string ToString() const;
+        JsonValue(map <string, JsonValue> arr2);
+
+
+        std::experimental::optional<JsonValue> ValueByName(const string &name) const;
+
+        string ToString() const;
 
 
     private:
-        JsonValue obj;
+        string str;
+        bool tf;
+        int numberInt;
+        double numberDouble;
+        map <string, JsonValue> arr;
+        vector <JsonValue> js;
     };
 }
 
