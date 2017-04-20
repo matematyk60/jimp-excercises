773adcec93fc4d0e7e713122603f6218d121fe83
matematyk60 <mleko321>
2017-04-20 19:45:08 +0200

    Added solution to Point3d exc in lab 7

---
+++ lab7/point3d/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by matematyk60 on 21.03.17.
+//
+#include <iostream>
+#include "../../lab7/point3d/Point3D.h"
+using namespace geometry;
+
+int main(){
+    Point3D p3D(12,31,12);//najpierw Konstruktor 2D, później kostruktor 3D
+    Point p2D(0,0);
+    std::cout << p2D.Distance(p3D); // następuje rzutowanie w doł Punktu 3D na punkt 2D
+    Point3D p3d(1,2,3);
+    std::cout << p3D << std::endl; // znów następuje rzutowanie w dół Punktu 3D na punkt 2D
+    // na koniec najpierw destruktor 3D, później 2D
+}
