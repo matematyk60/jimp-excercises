86979cba5cafadf3e8ca9c819933c64b8130be4d
Janek <ignatowicz@student.agh.edu.pl>
2017-05-08 13:33:42 +0200

    pesel works

--- lab8/pesel/Pesel.h
+++ lab8/pesel/Pesel.h
@@ -11,45 +11,46 @@ namespace academia {
     class Pesel {
     public:
         Pesel(const char *input);
+
         static void validatePESEL(const char *input);
+
         static bool PeselChecksum(const char *input);
+
         static bool PeselLength(const char *input);
+
         static bool PeselCharacter(const char *input);
     };
 
-    class AcademiaDataValidationError : public std::runtime_error{
+    class AcademiaDataValidationError : public std::runtime_error {
     public:
-        AcademiaDataValidationError(std::string message) : std::runtime_error(message){
-            message_ = message;
+        AcademiaDataValidationError(std::string message) : std::runtime_error(message) {
         }
-        virtual ~AcademiaDataValidationError(){};
+
+        virtual ~AcademiaDataValidationError() {};
     private:
         std::string message_;
     };
 
-    class InvalidPeselChecksum : public AcademiaDataValidationError{
+    class InvalidPeselChecksum : public AcademiaDataValidationError {
     public:
         InvalidPeselChecksum(const std::string &pesel = "PESEL", int checksum = 0);
-    private:
-        std::string message_;
     };
 
-    class InvalidPeselLength : public AcademiaDataValidationError{
+    class InvalidPeselLength : public AcademiaDataValidationError {
     public:
-        InvalidPeselLength(const std::string &pesel = "PESEL", int length = 0);
+        InvalidPeselLength(std::string = "PESEL", int length = 0);
+
     private:
         std::string message_;
     };
 
-    class InvalidPeselCharacter : public AcademiaDataValidationError{
+    class InvalidPeselCharacter : public AcademiaDataValidationError {
     public:
         InvalidPeselCharacter(const std::string &pesel = "PESEL");
+
     private:
         std::string message_;
     };
-
-
-
 }
 
 #endif //JIMP_EXERCISES_PESEL_H
