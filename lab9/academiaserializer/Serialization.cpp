8b5f6e301b6c70ba174fccdc48c233576ccefc8b
Janek <ignatowicz@student.agh.edu.pl>
2017-05-13 15:15:45 +0200

    serialozation - beggining

--- lab9/academiaserializer/Serialization.cpp
+++ lab9/academiaserializer/Serialization.cpp
@@ -4,3 +4,59 @@
 
 #include "Serialization.h"
 
+namespace academia {
+    Serializer::Serializer(ostream *out) {
+        out_ = out;
+    }
+
+    Room::Room(int id, const string &name, Room::Type type) {
+        id_ = id;
+        name_ = name;
+        type_ = type;
+    }
+
+    string Room::TypeToString(Room::Type type) const {
+        switch (type) {
+            case Type::COMPUTER_LAB:
+                return "COMPUTER_LAB";
+            case Type::CLASSROOM:
+                return "CLASSROOM";
+            case Type::LECTURE_HALL:
+                return "LECTURE_HALL";
+        }
+    }
+
+    void Room::Serialize(Serializer *serializer) const {
+        serializer->IntegerField("id", id_);
+        serializer->StringField("name", name_);
+        serializer->Header("room");
+        serializer->Footer("room");
+        serializer->StringField("type", TypeToString(type_));
+    }
+
+
+    string Building::TypeToString(Building::Type type) const {
+        switch (type) {
+            case Type::COMPUTER_LAB:
+                return "COMPUTER_LAB";
+            case Type::CLASSROOM:
+                return "CLASSROOM";
+            case Type::LECTURE_HALL:
+                return "LECTURE_HALL";
+        }
+    }
+
+    Building::Building(int id, const string &name, Building::Type type) {
+        id_ = id;
+        name_ = name;
+        type_ = type;
+    }
+
+    void Building::Serialize(Serializer *serializer) const {
+        serializer->IntegerField("id", id_);
+        serializer->StringField("name", name_);
+        serializer->Header("room");
+        serializer->Footer("room");
+        serializer->StringField("type", TypeToString(type_));
+    }
+}
--- lab9/academiaserializer/Serialization.h
+++ lab9/academiaserializer/Serialization.h
@@ -5,7 +5,144 @@
 #ifndef JIMP_EXERCISES_SERIALIZATION_H
 #define JIMP_EXERCISES_SERIALIZATION_H
 
+#include <string>
+#include <vector>
+#include <functional>
+#include <ostream>
+
+using namespace std;
+using namespace std::literals;
+
+namespace academia {
+
+    class Serializer;
+
+    class Serializable;
+
+    class Room;
+
+    class XmlSerializer;
 
 
+    class Serializer {
+    public:
+        Serializer(ostream *out);
 
-#endif //JIMP_EXERCISES_SERIALIZATION_H
+        virtual void IntegerField(const std::string &field_name, int value) = 0;
+
+        virtual void DoubleField(const std::string &field_name, double value) = 0;
+
+        virtual void StringField(const std::string &field_name, const std::string &value) = 0;
+
+        virtual void BooleanField(const std::string &field_name, bool value) = 0;
+
+        virtual void SerializableField(const std::string &field_name, const academia::Serializable &value) = 0;
+
+        virtual void ArrayField(const std::string &field_name,
+                                const vector<reference_wrapper<const academia::Serializable>> &value) = 0;
+
+        virtual void Header(const std::string &object_name) = 0;
+
+        virtual void Footer(const std::string &object_name) = 0;
+
+    protected:
+        ostream *out_;
+    };
+
+
+    class Serializable {
+    public:
+        virtual void Serialize(Serializer *serializer) const = 0;
+
+    };
+
+
+    class Room : public Serializable {
+    public:
+
+        enum Type {
+            COMPUTER_LAB,
+            CLASSROOM,
+            LECTURE_HALL
+        };
+
+        string TypeToString(Type type) const;
+
+        Room(int id, const string &name, Type type);
+
+        void Serialize(Serializer *serializer) const override;
+
+    private:
+        int id_;
+        string name_;
+        Type type_;
+    };
+
+
+    class Building{
+    public:
+
+        enum Type {
+            COMPUTER_LAB,
+            CLASSROOM,
+            LECTURE_HALL
+        };
+
+        string TypeToString(Type type) const;
+
+        Building(int id, const string &name, Type type);
+
+
+    private:
+        int id_;
+        string name_;
+        Type type_;
+    };
+
+
+    class XmlSerializer : public Serializer {
+        XmlSerializer(std::ostream *out) : Serializer(out) {}
+
+        void IntegerField(const std::string &field_name, int value) override {
+            *out_ << "<" + field_name + ">" + to_string(value) + "<\\" + field_name + ">";
+        }
+
+        void DoubleField(const std::string &field_name, double value) override {
+            *out_ << "<" + field_name + ">" + to_string(value) + "<\\" + field_name + ">";
+        }
+
+        void StringField(const std::string &field_name, const std::string &value) override {
+            *out_ << "<" + field_name + ">" + value + "<\\" + field_name + ">";
+        }
+
+        void BooleanField(const std::string &field_name, bool value) override {
+            *out_ << "<" + field_name + ">" + to_string(value) + "<\\" + field_name + ">";
+        }
+
+        void SerializableField(const std::string &field_name, const academia::Serializable &value) override {
+            *out_ << "<" + field_name + ">";
+            value.Serialize(this);
+            *out_ << "<\\" + field_name + ">";
+        }
+
+        void ArrayField(const std::string &field_name,
+                        const vector<reference_wrapper<const academia::Serializable>> &value) override {
+            Header(field_name);
+            for (const Serializable &n : value)
+                n.Serialize(this);
+            Footer(field_name);
+        }
+
+        void Header(const std::string &object_name) override {
+            *out_ << "<" + object_name + ">";
+        }
+
+        void Footer(const std::string &object_name) override {
+            *out_ << "<" + object_name + ">";
+        }
+
+    };
+}
+
+
+#endif //JIMP_EXERCISES_SERIALIZATION_Hz
