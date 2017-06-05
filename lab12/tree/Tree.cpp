20a0491542ba4300f14a4f695b4a00abbf940ba5
Jan Ignatowicz <ignatowicz@student.agh.edu.pl>
2017-06-05 12:33:23 +0200

    drzewko

---
+++ lab12/tree/Tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by janek on 30.05.17.
+//
+
+#include "Tree.h"
---
+++ lab12/tree/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by janek on 30.05.17.
+//
+
+#include "Tree.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace tree;
+
+int main(){
+//    działą:
+    Tree<int> first;
+//    co to jest:
+//    Tree<int> firsta(); // dlaczego nie mogę na first operować?
+
+//    to działą:
+//    Node<int> nodzik(5);
+//    Node<int>(8);
+//    std::make_unique<Node<int>>(9);
+
+
+    return 0;
+}
